2ea67f927009a197bcfced2b6dbcad0f349cefe6
autoclcker <nil>
2025-07-27 01:26:05 +0300

    feat: nvim bindings

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("i", "<C-f>", "<Right>", { desc = "Move cursor right in insert mode" })
+vim.keymap.set("i", "<C-b>", "<Left>", { desc = "Move cursor left in insert mode" })
---
+++ .config/nvim/lua/plugins/blink.lua
@@ -0,0 +1,124 @@
+return {
+  "saghen/blink.cmp",
+  version = not vim.g.lazyvim_blink_main and "*",
+  build = vim.g.lazyvim_blink_main and "cargo build --release",
+  opts_extend = {
+    "sources.completion.enabled_providers",
+    "sources.compat",
+    "sources.default",
+  },
+  dependencies = {
+    "rafamadriz/friendly-snippets",
+    -- add blink.compat to dependencies
+    {
+      "saghen/blink.compat",
+      optional = true, -- make optional so it's only enabled if any extras need it
+      opts = {},
+      version = not vim.g.lazyvim_blink_main and "*",
+    },
+  },
+  event = "InsertEnter",
+
+  opts = {
+    snippets = {
+      expand = function(snippet, _)
+        return LazyVim.cmp.expand(snippet)
+      end,
+    },
+    appearance = {
+      use_nvim_cmp_as_default = false,
+      nerd_font_variant = "mono",
+    },
+    completion = {
+      accept = {
+        auto_brackets = {
+          enabled = true,
+        },
+      },
+      menu = {
+        draw = {
+          treesitter = { "lsp" },
+        },
+      },
+      documentation = {
+        auto_show = true,
+        auto_show_delay_ms = 200,
+      },
+      ghost_text = {
+        enabled = vim.g.ai_cmp,
+      },
+    },
+
+    sources = {
+      compat = {},
+      default = { "lsp", "path", "snippets", "buffer" },
+    },
+
+    cmdline = {
+      enabled = false,
+    },
+
+    keymap = {
+      preset = "enter",
+      ["<C-y>"] = false,
+      ["<Tab>"] = { "select_and_accept" },
+    },
+  },
+  ---@param opts blink.cmp.Config | { sources: { compat: string[] } }
+  config = function(_, opts)
+    local enabled = opts.sources.default
+    for _, source in ipairs(opts.sources.compat or {}) do
+      opts.sources.providers[source] = vim.tbl_deep_extend(
+        "force",
+        { name = source, module = "blink.compat.source" },
+        opts.sources.providers[source] or {}
+      )
+      if type(enabled) == "table" and not vim.tbl_contains(enabled, source) then
+        table.insert(enabled, source)
+      end
+    end
+
+    -- add ai_accept to <Tab> key
+    if not opts.keymap["<Tab>"] then
+      if opts.keymap.preset == "super-tab" then -- super-tab
+        opts.keymap["<Tab>"] = {
+          require("blink.cmp.keymap.presets")["super-tab"]["<Tab>"][1],
+          LazyVim.cmp.map({ "snippet_forward", "ai_accept" }),
+          "fallback",
+        }
+      else -- other presets
+        opts.keymap["<Tab>"] = {
+          LazyVim.cmp.map({ "snippet_forward", "ai_accept" }),
+          "fallback",
+        }
+      end
+    end
+
+    -- Unset custom prop to pass blink.cmp validation
+    opts.sources.compat = nil
+
+    for _, provider in pairs(opts.sources.providers or {}) do
+      if provider.kind then
+        local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
+        local kind_idx = #CompletionItemKind + 1
+
+        CompletionItemKind[kind_idx] = provider.kind
+        CompletionItemKind[provider.kind] = kind_idx
+
+        local transform_items = provider.transform_items
+        provider.transform_items = function(ctx, items)
+          items = transform_items and transform_items(ctx, items) or items
+          for _, item in ipairs(items) do
+            item.kind = kind_idx or item.kind
+            item.kind_icon = LazyVim.config.icons.kinds[item.kind_name] or item.kind_icon or nil
+          end
+          return items
+        end
+
+        provider.kind = nil
+      end
+    end
+
+    require("blink.cmp").setup(opts)
+  end,
+}
