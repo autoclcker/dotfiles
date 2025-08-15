697e7d2e57b668f6526f6323ae703ca73b92279d
autoclcker <nil>
2025-08-15 07:14:23 +0300

    feat(copyq): add buffer tab

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -4,9 +4,11 @@
 
 vim.keymap.set("c", "<C-a>", "<Home>", { desc = "Move cursor to the beginning of the line start in command mode" })
 vim.keymap.set("c", "<C-e>", "<End>", { desc = "Move cursor to the end of the line in command mode" })
-vim.keymap.set("c", "<C-f>", "<Right>", { desc = "Move cursor right in command mode" })
-vim.keymap.set("c", "<C-b>", "<Left>", { desc = "Move cursor left in command mode" })
 vim.keymap.set("i", "<C-f>", "<Right>", { desc = "Move cursor right in insert mode" })
 vim.keymap.set("i", "<C-b>", "<Left>", { desc = "Move cursor left in insert mode" })
 vim.keymap.set("i", "<M-f>", "<C-Right>", { desc = "Move one word forward in insert mode" })
 vim.keymap.set("i", "<M-b>", "<C-Left>", { desc = "Move one word backward in insert mode" })
+vim.keymap.del("n", "<C-l>")
+vim.keymap.del("n", "<C-k>")
+vim.keymap.del("n", "<C-j>")
+vim.keymap.del("n", "<C-h>")
