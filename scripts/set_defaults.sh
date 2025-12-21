212a15d8540178c74efe29e3dbfd46fa98f0d6ec
autoclcker <nil>
2025-12-21 22:05:16 +0300

    feat: new tmux-easymotion implementation
    untested

--- scripts/set_defaults.sh
+++ scripts/set_defaults.sh
@@ -4,11 +4,13 @@
 source "scripts/helpers.sh"
 
 XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-"$HOME/.config"}
+TMUX_PLUGINS_HOME=${TMUX_PLUGINS_HOME:-"$XDG_CONFIG_HOME/tmux/plugins"}
 ZSH_PLUGINS_HOME=${ZSH_PLUGINS_HOME:-"$HOME/.oh-my-zsh/custom/plugins"}
 
 CHEATSHEETS_REPO=${CHEATSHEETS_REPO:-"https://github.com/cheat/cheatsheets.git"}
 HELM_DIFF_REPO=${HELM_DIFF_REPO:-"https://github.com/databus23/helm-diff"}
 NERD_FONTS_REPO=${NERD_FONTS_REPO:-"https://github.com/ryanoasis/nerd-fonts.git"}
+TMUX_EASYMOTION_REPO=${TMUX_EASYMOTION_REPO:-"https://github.com/ddzero2c/tmux-easymotion.git"}
 ZSH_AUTOSUGGESTIONS_REPO=${ZSH_AUTOSUGGESTIONS_REPO:-"https://github.com/zsh-users/zsh-autosuggestions.git"}
 ZSH_SYNTAX_HIGHLIGHTING_REPO=${ZSH_SYNTAX_HIGHLIGHTING_REPO:-"https://github.com/zsh-users/zsh-syntax-highlighting.git"}
 
@@ -75,6 +77,14 @@ return {
 EOF
 fi
 log "${CYAN}" "Yazi is configured\n"
+
+# Tmux
+if [[ $(tmux --version) ]] && [[ ! -d "${TMUX_PLUGINS_HOME}/tmux-easymotion" ]]; then
+  mkdir --parents "$TMUX_PLUGINS_HOME"
+  git clone --depth 1 "${TMUX_EASYMOTION_REPO}" "${TMUX_PLUGINS_HOME:-$TMUX_PLUGINS_HOME}/tmux-easymotion"
+else
+  log "${CYAN}" "Tmux plugins are already installed\n"
+fi
 
 # Zsh
 if [[ $(zsh --version) ]] && [[ ! -d "${ZSH_PLUGINS_HOME}/zsh-autosuggestions" ]]; then
