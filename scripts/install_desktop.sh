0c5d1b4acd053d03bed0a5152c525f4374f46212
autoclcker <nil>
2025-12-28 17:11:53 +0300

    fix: LIBVA_DRIVER_NAME no longer harms browser calls
    untested

--- scripts/install_desktop.sh
+++ scripts/install_desktop.sh
@@ -34,7 +34,6 @@ if grep --quiet "__NV_PRIME_RENDER_OFFLOAD" /etc/environment; then
   log "${CYAN}" "Nvidia is already configured\n"
 else
   sudo tee --append /etc/environment <<EOF
-LIBVA_DRIVER_NAME=nvidia
 __NV_PRIME_RENDER_OFFLOAD=1
 __GLX_VENDOR_LIBRARY_NAME=nvidia
 EOF
