c05ae52052bbafa44413d263a4d4c7d6386efef8
autoclcker <nil>
2026-03-21 17:32:30 +0300

    feat(editors): yazi integration

---
+++ .config/nvim/lua/plugins/yazi.lua
@@ -0,0 +1,35 @@
+return {
+  "mikavilpas/yazi.nvim",
+  version = "*",
+  event = "VeryLazy",
+  dependencies = {
+    { "nvim-lua/plenary.nvim", lazy = true },
+  },
+  keys = {
+    {
+      "<leader>y",
+      mode = { "n", "v" },
+      "<cmd>Yazi<cr>",
+      desc = "Open yazi at the current file",
+    },
+    {
+      "<leader>cw",
+      "<cmd>Yazi cwd<cr>",
+      desc = "Open the file manager in nvim's working directory",
+    },
+    {
+      "<c-up>",
+      "<cmd>Yazi toggle<cr>",
+      desc = "Resume the last yazi session",
+    },
+  },
+  opts = {
+    open_for_directories = false,
+    keymaps = {
+      open_file_in_horizontal_split = "<c-s>",
+    },
+  },
+  init = function()
+    vim.g.loaded_netrwPlugin = 1
+  end,
+}
